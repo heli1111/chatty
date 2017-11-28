19e7699b47952972e0db2073f169019fbb2d7115
Jessica <hepu24@gmail.com>
2017-11-28 16:28:05 +0000

    add html into jsx format

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import Nav from './Nav.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Nav />
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
-}
+}s
 export default App;
---
+++ src/MessageList.jsx
@@ -0,0 +1,15 @@
+// Message List
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+        <main className="messages">
+          <Message />
+        </main>
+    )
+  }
+}
+
+export default MessageList;
