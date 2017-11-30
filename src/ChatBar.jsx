7455ba539fe5cb2794e0178aaffbcaa4cc57ea4b
Jessica <hepu24@gmail.com>
2017-11-30 19:31:59 +0000

    changing username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -5,7 +5,7 @@ class ChatBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: props.currentUser.name,
+      username: props.username,
       message: ''
     };
   }
